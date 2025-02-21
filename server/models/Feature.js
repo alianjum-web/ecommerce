5f8e0ad545ce827fa10d3810b9809f06e34d50d9
alianjum-web <muhammadalikhanzada7890@gmail.com>
2025-02-21 16:53:34 +0500

    add fields to model

--- server/models/Feature.js
+++ server/models/Feature.js
@@ -1,10 +1,8 @@
 import mongoose from "mongoose";
 
-const FeatureSchema = new Schema(
-  {
-    image: String
-  },
-  { timestamps: true }
-);
+const FeatureSchema = new mongoose.Schema({
+  imageUrl: { type: String, required: true }, // Cloudinary image URL
+  imagePublicId: { type: String, required: true }, // Cloudinary public ID
+}, { timestamps: true });
 
 export const Feature = mongoose.model("Feature", FeatureSchema);
