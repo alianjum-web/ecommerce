21c237e930b27ce5f2f6a3027cccfc5216048109
alianjum-web <muhammadalikhanzada7890@gmail.com>
2025-03-28 14:26:41 +0500

    Upgrade Next.js to 15.2.4, add redux-persist for state management, and implement authentication slice with async actions for user registration, login, and session management.

--- frontend/store/store.ts
+++ frontend/store/store.ts
@@ -1,9 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
 import authReducer from "./auth-slice";
 import adminProductsSlice from "./admin/products-slice";
 import adminOrderSlice from "./admin/order-slice";
 
-import shopProductsSlice from "./shop/products-slice";
+import shopProductsSlice from "./shop/product-slice";
 import shopCartSlice from "./shop/cart-slice";
 import shopAddressSlice from "./shop/address-slice";
 import shopOrderSlice from "./shop/order-slice";
@@ -11,7 +14,8 @@ import shopSearchSlice from "./shop/search-slice";
 import shopReviewSlice from "./shop/review-slice";
 import commonFeatureSlice from "./common-slice";
 
-const store = configureStore({
+
+const rootReducer = combineReducers({
   reducer: {
     auth: authReducer,
 
@@ -29,4 +33,24 @@ const store = configureStore({
   },
 });
 
-export default store;
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth", "shopCart", "shopAddresses"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+    devTools: process.env.NODE_ENV !== "production",
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
